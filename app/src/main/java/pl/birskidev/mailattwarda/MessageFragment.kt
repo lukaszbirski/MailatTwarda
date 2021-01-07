98f9ec1aa2f623446629dcc9223da3d578e9a89e
lukasz <lukasz.birski@gmail.com>
2021-01-07 10:29:07 +0100

    Created basic navigation architecture

---
+++ app/src/main/java/pl/birskidev/mailattwarda/MessageFragment.kt
@@ -0,0 +1,19 @@
+package pl.birskidev.mailattwarda
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MessageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.message_fragment, container, false)
+    }
+
+}
