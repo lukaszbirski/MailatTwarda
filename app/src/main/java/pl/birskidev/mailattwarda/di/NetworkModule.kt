2a86da72d373afedc569490ad736f05e323a2f9f
lukasz <lukasz.birski@gmail.com>
2021-01-07 12:32:27 +0100

    Corrected display of email address, date and time in recyclerview

--- app/src/main/java/pl/birskidev/mailattwarda/di/NetworkModule.kt
+++ app/src/main/java/pl/birskidev/mailattwarda/di/NetworkModule.kt
@@ -5,6 +5,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import pl.birskidev.mailattwarda.network.mapper.MessageDtoMapper
+import pl.birskidev.mailattwarda.network.mapper.util.MyMessageUtil
 import pl.birskidev.mailattwarda.network.response.FetchMails
 import pl.birskidev.mailattwarda.network.response.FetchMailsImpl
 import javax.inject.Singleton
@@ -15,14 +16,20 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideMessageDtoMapper() : MessageDtoMapper {
-        return MessageDtoMapper()
+    fun provideMessageDtoMapper(myMessageUtil: MyMessageUtil) : MessageDtoMapper {
+        return MessageDtoMapper(myMessageUtil)
     }
 
     @Singleton
     @Provides
     fun provideFetchMails() : FetchMails {
         return FetchMailsImpl()
+    }
+
+    @Singleton
+    @Provides
+    fun provideMyMessageUtil() : MyMessageUtil {
+        return MyMessageUtil()
     }
 
 }
