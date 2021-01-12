b0a5a78c0d5339f10f8bcbcf4b3e1b4a8d370867
lukasz <lukasz.birski@gmail.com>
2021-01-12 21:04:26 +0100

    Created adapter for chips

---
+++ app/src/main/java/pl/birskidev/mailattwarda/presentation/ui/message_list/adapter/ChipAdapter.kt
@@ -0,0 +1,37 @@
+package pl.birskidev.mailattwarda.presentation.ui.message_list.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import pl.birskidev.mailattwarda.R
+import pl.birskidev.mailattwarda.databinding.ItemChipBinding
+import pl.birskidev.mailattwarda.domain.model.MyChip
+
+class ChipAdapter(
+        private val chips: List<MyChip>
+) : RecyclerView.Adapter<ChipAdapter.ChipViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+            ChipViewHolder(
+                    DataBindingUtil.inflate<ItemChipBinding>(
+                            LayoutInflater.from(parent.context),
+                            R.layout.item_chip,
+                            parent,
+                            false
+                    )
+            )
+
+    override fun onBindViewHolder(holder: ChipViewHolder, position: Int) {
+        holder.recyclerViewMyChipBinding.chip = chips[position]
+    }
+
+    override fun getItemCount() = chips.size
+
+    inner class ChipViewHolder(
+            val recyclerViewMyChipBinding: ItemChipBinding
+    ) : RecyclerView.ViewHolder(recyclerViewMyChipBinding.root) {
+
+    }
+
+}
