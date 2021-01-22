362532f23e599dc68585775770342b52d4330d49
lukasz <lukasz.birski@gmail.com>
2021-01-22 18:43:20 +0100

    Added app center

--- app/src/main/java/pl/birskidev/mailattwarda/presentation/MainActivity.kt
+++ app/src/main/java/pl/birskidev/mailattwarda/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
 package pl.birskidev.mailattwarda.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.StrictMode
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import dagger.hilt.android.AndroidEntryPoint
 import pl.birskidev.mailattwarda.R
 
@@ -14,5 +17,10 @@ class MainActivity : AppCompatActivity() {
 
         var policy: StrictMode.ThreadPolicy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
+
+        AppCenter.start(
+            application, "53a5ece4-4e87-43c4-a20b-bbaea7e77943",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
