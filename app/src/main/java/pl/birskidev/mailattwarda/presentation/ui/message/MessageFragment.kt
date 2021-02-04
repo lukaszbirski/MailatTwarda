4d5590746b8eddcd54f35c7e9cc8c434501aefef
lukasz <lukasz.birski@gmail.com>
2021-02-04 18:27:06 +0100

    Removed unnecessary persmissions

--- app/src/main/java/pl/birskidev/mailattwarda/presentation/ui/message/MessageFragment.kt
+++ app/src/main/java/pl/birskidev/mailattwarda/presentation/ui/message/MessageFragment.kt
@@ -1,22 +1,13 @@
 package pl.birskidev.mailattwarda.presentation.ui.message
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import pl.birskidev.mailattwarda.databinding.MessageFragmentBinding
-import pl.birskidev.mailattwarda.domain.model.MyMessage
-
-private val STORAGE_PERMISSION_CODE: Int = 1000
 
 @AndroidEntryPoint
 class MessageFragment : Fragment() {
@@ -49,22 +40,6 @@ class MessageFragment : Fragment() {
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        when (requestCode) {
-            STORAGE_PERMISSION_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    view?.let { viewModel.downloadMessages(it) }
-                } else {
-                    Toast.makeText(context, "ERROR", Toast.LENGTH_LONG).show()
-                }
-            }
-        }
     }
 
 }
