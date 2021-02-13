1c1f3c65f737e60871d84c70959da06f195d0efa
lukasz <lukasz.birski@gmail.com>
2021-02-13 14:20:02 +0100

    Fixed presse back button when dialog is showed

--- app/src/main/java/pl/birskidev/mailattwarda/presentation/MainActivity.kt
+++ app/src/main/java/pl/birskidev/mailattwarda/presentation/MainActivity.kt
@@ -59,6 +59,7 @@ class MainActivity : AppCompatActivity() {
         dialogBuilder.setView(view)
         dialog = dialogBuilder.create()
         dialog.setCanceledOnTouchOutside(false)
+        dialog.setCancelable(false)
         dialog.show()
     }
 
